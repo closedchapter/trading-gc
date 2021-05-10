595e72ecd663634f5dd2ce5faae8fa9ff1cfea25
horridharry <79926700+horridharry@users.noreply.github.com>
2021-05-10 18:16:51 +0100

    Commit

--- src/components/renders/renderApply.js
+++ src/components/renders/renderApply.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from 'react-router-dom';
-import { db } from "../../firebase";
+//import { db } from "../../firebase";
 //import Data from '../data/apply.json';
 
 const Form = () => {
