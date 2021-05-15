6dcdf4cd97a5297a137eac62f6adc3f0a7872b17
horridharry <79926700+horridharry@users.noreply.github.com>
2021-05-15 23:11:39 +0100

    commit

--- src/components/renders/renderHome.js
+++ src/components/renders/renderHome.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import moment from 'moment';
 import { db } from "../../firebase";
 
 const DropDown = (uid) => {
@@ -58,8 +59,8 @@ const DropDown = (uid) => {
 
 
     return(
-        <div ref={drop} className='ml-auto'>
-            <button className='hover:text-black hover:bg-gray-100 rounded px-3 -mr-2' onClick={() => setOpen(open => !open)}><svg class="w-6 h-6 my-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path></svg></button>
+        <div ref={drop} className='dropdown'>
+            <button className='hover:text-black hover:bg-gray-100 rounded-xl p-2' onClick={() => setOpen(open => !open)}><svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path></svg></button>
             {open && <Modal uid={uid}/>}
         </div>
     );
@@ -89,25 +90,45 @@ const DropDown = (uid) => {
         return () => update();
     }, []);
 
+    const getTime = (uid) => {
+        moment().format();
+        return (
+            moment.utc(uid).local().startOf('seconds').fromNow()
+        )
+    }
+
     const item = data?.map((data) =>
-        <div key={data.uid} className='bg-white rounded-2xl ring-1 ring-gray-300 py-2 px-4 mb-4'>
-            <div className='inline-flex text-gray-400 w-full'>
-                <div className='my-2 text-base'>@{data.author}</div>
-                <DropDown uid={data.uid}/>
+        <div key={data.uid} className='flex flex-col bg-white rounded-lg mb-5 p-5'>
+
+            <div className='inline-flex items-center text-base mb-5'>
+                <div className='relative bg-gray-300 rounded-full w-max p-5'>
+                    <div className='absolute -m-3 text-base text-gray-400'>🚀</div>
+                </div>
+                <div className='ml-4'><b>{data.author}</b></div>
+                <div className='ml-auto'><DropDown uid={data.uid}/></div>
             </div>
-            <div className='my-2'>{data.message}</div>
+
+            <div className='flex flex-col w-full'>
+                <div className='mb-7'>{data.message}</div>
+                <div className='inline-flex text-gray-400 items-center text-base'>
+                    <div className=''>{getTime(data.uid)}</div>
+                </div>
+            </div>
+        
         </div>
     );
 
     return (
         <div className='p-4 text-lg font-medium leading-relax h-screen'>
 
-            <div className='bg-gradient-to-tl from-purple-700 via-pink-500 to-red-500 p-4 -m-4 text-white pt-12 mb-4'>
-                <div className='font-bold text-3xl'>Trending🔥</div>
-                <div className='font-bold text-base mb-10'>Just in from our analysts</div>
+            <div className='bg-gradient-to-tl from-purple-700 via-pink-500 to-red-500 p-4 -m-4 text-white pt-16 mb-4'>
+                <div className='font-bold text-3xl'>Forecast🧐</div>
+                <div className='font-bold text-base mb-8'>Latest predictions from our analysts</div>
             </div>
 
-            <div className='rounded-3xl bg-white -m-4 -my-10 p-4 pt-8'>{item}</div>
+            <div className='rounded-xl bg-white -m-4 -my-10 p-4 pt-5 bg-gray-100'>
+                {item}
+            </div>
         </div>
     );
 }
--- src/components/renders/renderNav.js
+++ src/components/renders/renderNav.js
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from 'react-router-dom';
 
+const RenderModal = () => {
+    const [open, setOpen] = useState(false);
+    const drop = useRef(null);
+
+    const handleClick = (e) => {
+        if (open && !e.target.closest(`${drop.current.className}`)) {
+          setOpen(false);
+        }
+      }
+
+    const Modal = () => {
+            return (
+                <div className='fixed bg-white border p-5'>
+                    <div className=''></div>
+                </div>
+            );
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", handleClick);
+        return () => {
+          document.removeEventListener("click", handleClick);
+        };
+    });
+
+
+    return(
+        <div ref={drop} className='flex'>
+            <button onClick={() => setOpen(open => !open)}><svg class="w-8 h-8 my-auto mx-2 p-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4 6h16M4 12h16M4 18h7"></path></svg></button>
+            {open && <Modal />}
+        </div>
+    );
+}
+
 const Render = () => {
+
     return (
         <div className='relative'>
             <div className='ring-1 ring-gray-300 bg-white fixed w-full inline-flex justify-end py-3 px-2'>
-            <button><svg class="w-8 h-8 my-auto mx-2 p-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4 6h16M4 12h16M4 18h7"></path></svg></button>
+                <RenderModal />
                 <div className='items-start mr-auto font-bold text-xl my-auto'>Investors</div>
-                <Link to="/create" className='mx-3 items-start cursor-pointer bg-indigo-800 text-white font-semibold text-center rounded px-4 py-2'>Create</Link>
+                <Link to="/create" className='mx-3 items-start cursor-pointer bg-indigo-600 hover:bg-indigo-800 text-white font-semibold text-center rounded px-4 py-2'>Create</Link>
             </div>
             <div className='h-16'></div>
         </div>
